d461d20ba6d9442494d632978c99ecd5753a35e2
johntyc10 <johnchan070507@gmail.com>
2026-07-26 06:01:13 +0800

    Add solution counts (slower than just one solution)

--- poople_solver.py
+++ poople_solver.py
@@ -22,27 +22,33 @@ class PoopleSolver():
     def get_possible_next_words(self, word: str) -> list[str]:
         return [w for w in self.all_words if self.is_exactly_one_letter_apart(word, w)]
 
-    def solve(self) -> list[str]:
+    def solve(self) -> list[list[str]]:
         if self.start_word == self.target_word:
             return [self.target_word]
 
         tree = [[self.start_word]]
         visited = {self.start_word}
+        last_visited = set()
+        solutions = []  # may have more than one solution
         layer = 1
-        while True:
+        while not solutions:
             print(f"-> Looking through layer {layer} ({len(tree)} words)...")
             new_tree = []
             for word_history in tree:
                 possible_next_words = self.get_possible_next_words(word_history[-1])
                 if self.target_word in possible_next_words:
-                    return word_history + [self.target_word]
+                    solutions.append(word_history + [self.target_word])
                 for w in possible_next_words:
                     if w not in visited:
                         new_tree.append(word_history + [w])
-                    visited.add(w)
+                    last_visited.add(w)
 
+            visited = visited.union(last_visited)
+            last_visited = set()
             tree = copy.deepcopy(new_tree)
             layer += 1
+
+        return solutions
 
     def play(self):
         print("===== POOPLE SOLVER =====")
@@ -69,12 +75,12 @@ class PoopleSolver():
         # Start solving
         print("Solving poople...")
 
-        solution = self.solve()
-        print("Solution found.")
-        print("Solution:")
-        for i in range(len(solution)):
-            print(f"{i}: {solution[i]}")
-        print(f"The best solution takes {len(solution) - 1} guesses.")
+        solutions = self.solve()
+        print(f"{len(solutions)} solutions found.")
+        print("First solution:")
+        for i in range(len(solutions[0])):
+            print(f"{i}: {solutions[0][i]}")
+        print(f"The best solution takes {len(solutions[0]) - 1} guesses.")
 
 
 if __name__ == "__main__":
